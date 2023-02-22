910a83f2c69ff8de2553039faaa0afd10b751171
David Shure <shuredavid@gmail.com>
2023-02-21 23:28:07 -0600

    Initial commit

---
+++ src/grid.rs
@@ -0,0 +1,72 @@
+use bevy::prelude::*;
+
+#[derive(Debug, Resource)]
+pub struct SearchableGrid {
+    pub grid: Vec<Vec<GridCell>>,
+}
+
+#[derive(Resource, Component)]
+pub struct GridCellLabel;
+
+#[derive(Debug, Clone, Copy, Resource)]
+pub struct GridCell {
+    pub typ: GridCellType,
+    pub row: i32,
+    pub col: i32,
+}
+
+#[derive(Debug, Clone, Copy, Resource, Reflect, Eq, PartialEq)]
+pub enum GridCellType {
+    Wall,
+    Empty,
+}
+
+impl SearchableGrid {
+    pub fn new(rows: i32, cols: i32) -> SearchableGrid {
+        // Initialize
+        let mut grid: Vec<Vec<GridCell>> = Vec::new();
+
+        for i in 0..rows {
+            let slab: Vec<GridCell> = Vec::new();
+            grid.push(slab);
+
+            for j in 0..cols {
+                grid[i as usize].push(GridCell {
+                    typ: GridCellType::Empty,
+                    row: i,
+                    col: j,
+                });
+            }
+        }
+
+        SearchableGrid { grid }
+    }
+}
+
+impl GridCell {
+    pub fn get_render_position(
+        &self,
+        screen_width: f32,
+        screen_height: f32,
+        rows: i32,
+        cols: i32,
+    ) -> (f32, f32) {
+        let width_per_row = screen_width / rows as f32;
+        let height_per_column = screen_height / cols as f32;
+
+        (
+            width_per_row * self.row as f32 - (screen_width / 2.),
+            height_per_column * self.col as f32 - (screen_height / 2.),
+        )
+    }
+}
+
+//                       -
+//                       ^
+//               (screen_height/2)
+//                       |
+// | <- (screen_width/2)-0-(screen_width/2) -> |
+//                       |
+//               (screen_height/2)
+//                       v
+//                       -
---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+mod grid;
+
+const WINDOW_WIDTH: i32 = 800;
+const WINDOW_HEIGHT: i32 = 800;
+
+fn main() {
+    let mut grid = grid::SearchableGrid::new(50, 50);
+
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "I am a window!".to_string(),
+                width: WINDOW_WIDTH as f32,
+                height: WINDOW_HEIGHT as f32,
+                ..default()
+            },
+            ..default()
+        }))
+        .insert_resource(grid)
+        .add_system(redraw_grid_on_change)
+        .add_system(mouse_button_input)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut windows: ResMut<Windows>,
+    mut grid: ResMut<grid::SearchableGrid>,
+) {
+    let window = windows.primary_mut();
+
+    println!("{} x {}", window.width(), window.height());
+
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn redraw_grid_on_change(
+    mut grid: ResMut<grid::SearchableGrid>,
+    mut mesh_query: Query<Entity, With<grid::GridCellLabel>>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    if grid.is_changed() {
+        println!("Grid changed!");
+
+        for entity in &mut mesh_query {
+            commands.entity(entity).despawn_recursive();
+        }
+
+        for row in grid.grid.iter() {
+            for col in row.iter() {
+                let (x, y) =
+                    col.get_render_position(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32, 50, 50);
+
+                let mut color = Color::WHITE;
+
+                println!("{:?}", color);
+
+                if col.typ == grid::GridCellType::Wall {
+                    color = Color::BLACK;
+                }
+
+                commands.spawn((
+                    MaterialMesh2dBundle {
+                        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+                        transform: Transform::default()
+                            .with_scale(Vec3::splat(12.))
+                            .with_translation(Vec3::new(x, y, 0.)),
+                        material: materials.add(ColorMaterial::from(color)),
+                        ..default()
+                    },
+                    grid::GridCellLabel,
+                ));
+            }
+        }
+    }
+}
+
+fn mouse_button_input(
+    buttons: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mut grid: ResMut<grid::SearchableGrid>,
+) {
+    // Games typically only have one window (the primary window).
+    // For multi-window applications, you need to use a specific window ID here.
+    let window = windows.get_primary().unwrap();
+
+    if buttons.pressed(MouseButton::Left) {
+        // Left button was pressed
+        if let Some(_position) = window.cursor_position() {
+            // cursor is inside the window, position given
+            println!("({},{})", _position.x, _position.y);
+        }
+    }
+}
